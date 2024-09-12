6443e943f6d30a67807a8b573374e8ec29aa56ae
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2024-09-12 22:35:35 +0300

    Add logic for one round

--- javascript.js
+++ javascript.js
@@ -10,4 +10,49 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    choice = prompt("Give your choice (rock, paper, or scissors): ");
+    return choice.toLowerCase();
+}
+
+humanScore = 0;
+computerScore = 0;
+
+function playRound() {
+    computerChoice = getComputerChoice();
+    humanChoice = getHumanChoice();
+
+    if (computerChoice === "rock") {
+        if (humanChoice === "rock") {
+            console.log("It's a tie!");
+        } else if (humanChoice === "paper") {
+            console.log("You win! Paper beats Rock!")
+            humanScore++;
+        } else {
+            console.log("You lose! Scissors lose to Rock!")
+            computerScore++;
+        }
+    else if (computerChoice === "paper") {
+        if (humanChoice === "paper") {
+            console.log("It's a tie!");
+        } else if (humanChoice === "scissors") {
+            console.log("You win! Scissors beats Paper!")
+            humanScore++;
+        } else {
+            console.log("You lose! Rock loses to Paper!")
+            computerScore++;
+        }
+    } else {
+        if (humanChoice === "scissors") {
+            console.log("It's a tie!");
+        } else if (humanChoice === "rock") {
+            console.log("You win! Rock beats Scissors!")
+            humanScore++;
+        } else {
+            console.log("You lose! Paper loses to Scissors!")
+            computerScore++;
+        }
+    }
+
+    
+   
